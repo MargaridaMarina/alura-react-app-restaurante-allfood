a1beb02203e770ebac0913d983fdfaf2c804e1f2
Margarida <margaridammcm@gmail.com>
2022-07-30 15:32:30 -0300

    Refatorando axios

--- restaurante_site/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ restaurante_site/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -2,6 +2,7 @@ import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, 
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import http from "../../../http"
 import IRestaurante from "../../../interfaces/IRestaurante"
 
 const AdministracaoRestaurantes = () => {
@@ -9,12 +10,12 @@ const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
 
   useEffect(() => {
-    axios.get<IRestaurante[]>('http://0.0.0.0:8000/api/v2/restaurantes/')
+    http.get<IRestaurante[]>('restaurantes/')
       .then(resposta => setRestaurantes(resposta.data))
   }, [])
 
   const excluir = (restauranteAserExcluido: IRestaurante) => {
-    axios.delete(`http://0.0.0.0:8000/api/v2/restaurantes/${restauranteAserExcluido.id}/`)
+    http.delete(`restaurantes/${restauranteAserExcluido.id}/`)
       .then(() => {
         const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAserExcluido.id)
         setRestaurantes([...listaRestaurante])
